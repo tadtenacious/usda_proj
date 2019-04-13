2eec669b0b1228c7058a2034759c520a64a66c98
Tad Peterson <tadworkswith@gmail.com>
2019-04-13 14:48:47 -0500

    completed script

--- model_evaluations.py
+++ model_evaluations.py
@@ -14,5 +14,13 @@ X.drop(drop_cols, axis=1)
 scorer = make_scorer(mean_absolute_error)
 
 models = [
-    ('RandomForrest', RandomForestRegressor(n_jobs=-1, n))
+    ('RandomForrest', RandomForestRegressor(n_jobs=-1, n_estimators=100, criterion='mae')),
+    ('LinearRegression', LinearRegression()),
+    ('LassoRegression', Lasso()),
+    ('RidgeRegression', Ridge())
 ]
+cv = KFold(n_splits=5, shuffle=True, random_state=101)
+
+for name, model in models:
+    scores = cross_val_score(model, X, y, scoring=scorer,n_jobs=-1)
+    print('{} Average MAE: {:.2f}'.format(name, np.mean(scores)))
