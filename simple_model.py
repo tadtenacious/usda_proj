a16f09b9ed3c080e971cac7bf41fd6bcd7520834
Tad Peterson <tadworkswith@gmail.com>
2019-04-15 17:53:39 -0500

    added the other models for the hell of it

--- simple_model.py
+++ simple_model.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold, cross_val_predict
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.metrics import mean_absolute_error
 
 from time import time
@@ -15,14 +16,26 @@ X = X[['fips', 'pct_diabetes_adults13']]
 
 cv = KFold(n_splits=5, shuffle=True, random_state=101)
 
-rf = RandomForestRegressor(n_jobs=-1, n_estimators=100, criterion='mae')
 
-start = time()
-oof_preds = cross_val_predict(rf, X, y, cv=cv, method='predict')
-end = time()
-diff = end - start
-td = timedelta(seconds=diff)
-print('Completed in {}'.format(td))
-mae = mean_absolute_error(y, oof_preds)
-print(
-    'MAE with only two columns that I believe should be taken out: {:.4f}'.format(mae))
+def run_model(model, data, target, cv):
+    start = time()
+    oof_preds = cross_val_predict(model, data, target, cv=cv, method='predict')
+    end = time()
+    diff = end - start
+    td = timedelta(seconds=diff)
+    print('Completed in {}'.format(td))
+    mae = mean_absolute_error(target, oof_preds)
+    return mae
+
+
+models = [
+    (RandomForestRegressor(n_jobs=-1, n_estimators=100, criterion='mae'), 'RandomForest'),
+    (LinearRegression(), 'LinearRegression'),
+    (Lasso(), 'LassoRegression'),
+    (Ridge(), 'RidgeRegression')
+]
+
+for model, name in models:
+    print(f'Starting {name}')
+    mae = run_model(model, X, y, cv)
+    print(f'{name} MAE: {mae:.4f}')
