d7ae96396f3b8e44e487fc36f2d96bfff90b6eb8
Tad Peterson <tadworkswith@gmail.com>
2019-04-25 18:40:46 -0500

    corrected query for null handling

--- clean_data.py
+++ clean_data.py
@@ -4,50 +4,55 @@ import pandas as pd
 import re
 
 xl = pd.ExcelFile('DataDownload.xls')
-sheets = xl.sheet_names[2:] # First two sheets are meta data
+sheets = xl.sheet_names[2:]  # First two sheets are meta data
 
 # sheets that we will merge/join using the FIPS column
 merge_sheets = [
-     'Supplemental Data - County',
-     'ACCESS',
-     'STORES',
-     'RESTAURANTS',
-     'ASSISTANCE',
-     'INSECURITY',
-     'PRICES_TAXES',
-     'LOCAL',
-     'HEALTH',
-     'SOCIOECONOMIC'
+    'Supplemental Data - County',
+    'ACCESS',
+    'STORES',
+    'RESTAURANTS',
+    'ASSISTANCE',
+    'INSECURITY',
+    'PRICES_TAXES',
+    'LOCAL',
+    'HEALTH',
+    'SOCIOECONOMIC'
 ]
 # The state sheet only has one row per state, will merge on State column
 state_only = 'Supplemental Data - State'
 
-dataframes = (pd.read_excel(xl,i).rename({'FIPS ':'FIPS'},axis=1) if i == 'Supplemental Data - County' else
-                pd.read_excel(xl,i).drop(['State','County'],axis=1) for i in merge_sheets)
-df = reduce(lambda x,y: x.merge(y,on='FIPS',how='left'), dataframes)
-df['State'] = df['State'].apply(lambda x: x.strip()) # strip whitespace from the States column to join
-states = pd.read_excel(xl,sheet_name=state_only).query("State==State") # NaN's are not equal to anything, so make sure there is a value for states because of that one stupid row
-df = df.merge(states,on='State',how='left')
+dataframes = (pd.read_excel(xl, i).rename({'FIPS ': 'FIPS'}, axis=1) if i == 'Supplemental Data - County' else
+              pd.read_excel(xl, i).drop(['State', 'County'], axis=1) for i in merge_sheets)
+df = reduce(lambda x, y: x.merge(y, on='FIPS', how='left'), dataframes)
+# strip whitespace from the States column to join
+df['State'] = df['State'].apply(lambda x: x.strip())
+# NaN's are not equal to anything, so make sure there is a value for states because of that one stupid row
+states = pd.read_excel(xl, sheet_name=state_only).query(
+    "pct_obese_adults13==pct_obese_adults13")
+df = df.merge(states, on='State', how='left')
 
 convert_columns = [
     '2010 Census Population',
-    'Population Estimate, 2011', 
+    'Population Estimate, 2011',
     'Population Estimate, 2012',
-    'Population Estimate, 2013', 
+    'Population Estimate, 2013',
     'Population Estimate, 2014',
-    'Population Estimate, 2015', 
+    'Population Estimate, 2015',
     'Population Estimate, 2016',
     'School Breakfast Program participants FY 2011',
     'School Breakfast Program participants, FY 2012',
 ]
+
 
 def converter(x):
     if x is np.nan:
         return x
     if type(x) != str:
         return x
-    no_comma = x.replace(',','')
-    return pd.to_numeric(no_comma, downcast='integer',errors='coerce')
+    no_comma = x.replace(',', '')
+    return pd.to_numeric(no_comma, downcast='integer', errors='coerce')
+
 
 for col in convert_columns:
     df[col] = df[col].apply(converter)
@@ -63,11 +68,13 @@ num_features = df.select_dtypes(exclude='object')
 
 df_num = num_features.fillna(num_features.median())
 
+
 def standardize_name(cname):
-    cname = re.sub(',','', cname)
+    cname = re.sub(',', '', cname)
     cname = cname.strip().lower()
-    cname = re.sub(r'\s+','_', cname)
+    cname = re.sub(r'\s+', '_', cname)
     return cname
+
 
 df_num.columns = df_num.columns.map(standardize_name)
 
